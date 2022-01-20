d87a0817c1211f97294d9eedc71107802f45559b
SergeyTyagushev <tyagushev@ooonpt.ru>
2022-01-20 16:30:59 +0300

    testing

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,65 @@
 
 #include <gtest/gtest.h>
+#include "crc16.h"
 
-TEST(hexImage, record_check) {
+TEST(hexImage, record_check_true) {
+
     // Arrange
-    
+    char buf[] = { 1, 0x2b, 0xe, 0x1, 0x1, 0, 0 };
+    crc16::usMBCRC16(buf, 5);
     // Act (empty for this test)
-
+   bool res = crc16::checkCRC(buf, sizeof buf);
     // Assert
-   ASSERT_TRUE(true);
+    ASSERT_TRUE(res);
+}
+
+TEST(hexImage, record_check_false_hi) {
+
+    // Arrange
+    char buf[] = { 1, 0x2b, 0xe, 0x1, 0x1, 0, 0 };
+    crc16::usMBCRC16(buf, 5);
+    buf[6] =0;
+    // Act (empty for this test)
+    bool res = crc16::checkCRC(buf, sizeof buf);
+    // Assert
+    ASSERT_FALSE(res);
+}
+
+
+TEST(hexImage, record_check_false_lo) {
+
+    // Arrange
+    char buf[] = { 1, 0x2b, 0xe, 0x1, 0x1, 0, 0 };
+    crc16::usMBCRC16(buf, 5);
+    buf[5] =0;
+    // Act (empty for this test)
+    bool res = crc16::checkCRC(buf, sizeof buf);
+    // Assert
+    ASSERT_FALSE(res);
+}
+
+TEST(hexImage, record_check_zero_size) {
+
+    // Arrange
+    char buf[] = { 1, 0x2b, 0xe, 0x1, 0x1, 0, 0 };
+    crc16::usMBCRC16(buf, 5);
+    buf[5] =0;
+    // Act (empty for this test)
+    bool res = crc16::checkCRC(buf, 0); // len is 0
+    // Assert
+    ASSERT_FALSE(res);
+}
+
+TEST(hexImage, record_check_min_size) {
+
+    // Arrange
+    char buf[] = { 1, 0x2b, 0xe, 0x1, 0x1, 0, 0 };
+    crc16::usMBCRC16(buf, 5);
+    buf[5] =0;
+    // Act (empty for this test)
+    bool res = crc16::checkCRC(buf, 3); // len is 0
+    // Assert
+    ASSERT_FALSE(res);
 }
 
 
